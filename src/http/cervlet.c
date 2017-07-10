88083219b8580a9f0e4d09b968cd3a306ddccbf7
Mat Meredith <matthew.meredith@metaswitch.com>
2017-07-10 12:57:20 +0100

    Remove all publicly visible references to AGPL and Tildeslash

--- src/http/cervlet.c
+++ src/http/cervlet.c
@@ -608,22 +608,14 @@ static void do_home(HttpRequest req, HttpResponse res) {
 static void do_about(HttpRequest req, HttpResponse res) {
         StringBuffer_append(res->outputbuffer,
                             "<html><head><title>about monit</title></head><body bgcolor=white>"
-                            "<br><h1><center><a href='http://mmonit.com/monit/'>"
-                            "monit " VERSION "</a></center></h1>");
+                            "<br><h1><center>"
+                            "monit " VERSION "</center></h1>");
         StringBuffer_append(res->outputbuffer,
                             "<ul>"
-                            "<li style='padding-bottom:10px;'>Copyright &copy; 2001-2016 <a "
-                            "href='http://tildeslash.com/'>Tildeslash Ltd"
+                            "<li style='padding-bottom:10px;'>Copyright &copy; 2017 <a "
+                            "href='http://metaswitch.com/'>Metaswitch Networks"
                             "</a>. All Rights Reserved.</li></ul>");
         StringBuffer_append(res->outputbuffer, "<hr size='1'>");
-        StringBuffer_append(res->outputbuffer,
-                            "<p>This program is free software; you can redistribute it and/or "
-                            "modify it under the terms of the GNU Affero General Public License version 3</p>"
-                            "<p>This program is distributed in the hope that it will be useful, but "
-                            "WITHOUT ANY WARRANTY; without even the implied warranty of "
-                            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
-                            "<a href='http://www.gnu.org/licenses/agpl.html'>"
-                            "GNU AFFERO GENERAL PUBLIC LICENSE</a> for more details.</p>");
         StringBuffer_append(res->outputbuffer,
                             "<center><p style='padding-top:20px;'>[<a href='.'>Back to Monit</a>]</p></body></html>");
 }
